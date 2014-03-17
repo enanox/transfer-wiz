158aac172a1bb70fc2da2414dcba1c2ff1e06375
Mathias Rodriguez <mathias.rodriguez@gmail.com>
2014-03-17 10:07:19 -0300

    Back to PhantomJS for unit tests

--- karma.conf.js
+++ karma.conf.js
@@ -46,7 +46,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Firefox'],
+    browsers: ['PhantomJS'],
     
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
